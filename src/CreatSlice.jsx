be0f0c1e37e6bad1626af9d779f0d4376c21acfd
iammix <k.mixios@gmail.com>
2024-07-07 14:22:11 +0300

    Final

--- src/CreatSlice.jsx
+++ src/CreatSlice.jsx
@@ -1,31 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const CreatSlice = createSlice({
-  name: 'cart',
-  initialState: {
-    items: [], // Initialize items as an empty array
-  },
-  reducers: {
-    addItem: (state, action) => {
-      const {name, image, cost} = action.payload;
-      const existingItem = state.items.find(item=>item.name === name);
-      if (existingItem) {
-        existingItem.quantity++;
-      } else {
-        state.items.push({name, image, cost, quantity:1});
-      }
+const initialState = {
+    items: [],
+};
+
+const CreatSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItem: (state, action) => {
+            const { id, name, image, cost } = action.payload;
+            const existingItem = state.items.find((item) => item.id === id);
+            if (existingItem) {
+                existingItem.quantity++;
+            } else {
+                state.items.push({ id, name, image, cost, quantity: 1 });
+            }
+        },
+        removeItem: (state, action) => {
+            const id = action.payload;
+            state.items = state.items.filter((item) => item.id !== id);
+        },
+        updateQuantity: (state, action) => {
+            const { plant, diff } = action.payload;
+            let itemToUpdate = state.items.find((item) => item.id === plant.id);
+            if (itemToUpdate && itemToUpdate.quantity + diff > 0) {
+                itemToUpdate.quantity += diff;
+            }
+        },
     },
-    removeItem: (state, action) => {
-      state.items = state.items.filter(item => item.name !== action.payload);
-    },
-    updateQuantity: (state, action) => {
-      const { name, quantity } = action.payload;
-      const itemToUpdate = state.items.find(item => item.name === name);
-      if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
-      }
-    },
-  },
 });
 
 export const { addItem, removeItem, updateQuantity } = CreatSlice.actions;
